ec72bcb2aac868b44e81b92f49f53d98f0cfb248
mameli <mameli93@gmail.com>
2017-01-21 01:14:16 +0100

    plot

--- src/main.py
+++ src/main.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     pos=nx.spring_layout(graph,scale=2)
     print graph.nodes()
     nx.draw_networkx_nodes(graph,pos,
-                       node_color="#4286f4",
+                       node_color='#42f4dc',
                        node_size=600,
                        alpha=0.8)
     nx.draw_networkx_edges(graph,pos,width=1.0,alpha=0.5)
     nx.draw_networkx_labels(graph,pos,font_size=16)
+    #nx.write_gexf("output.gexf")
     converter = ENFConverter()
     converter.convert("a FU (b & c)")
     modelChecker = CTLModelChecker(path, 'TRUE & b')
@@ -21,7 +22,9 @@ if __name__ == "__main__":
     result = modelChecker.iterativeCheckFormula()
     nx.draw_networkx_nodes(graph,pos,
                        result[1],
-                       node_color="#42f46e",
+                       node_color='#42f46e',
                        node_size=600,
                        alpha=0.8)
-    plt.show()
+    plt.figure(1)
+    plt.axis('off')
+    plt.savefig("edge_colormap.png")
